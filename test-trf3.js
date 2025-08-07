e03aeda17ccc2984aabfa320b0a89ffab19059b0
Anna Lívia <157286722+Annalivia19@users.noreply.github.com>
2025-08-07 12:50:11 -0300

    Update test-trf3.js

--- test-trf3.js
+++ test-trf3.js
@@ -2,17 +2,17 @@ import path from 'path';
 import { executar } from './executarAutomacaoTRF3.js';
 
 const dadosMock = {
-  login: '10538268859',
-  senha: 'Ra241216',
-  numero_processo: '5004970-71.2024.4.03.6183',
+  login: 'xxxxxxx',
+  senha: 'xxxxxxxx',
+  numero_processo: '50xxx-xx.xxx24.x.xx.xxx3',
   peticao: {
     tipo: '5',
     descricao: 'Petição inicial TRF3',
-    path: path.resolve('/home/user/Downloads/peticao.pdf')
+    path: path.resolve('/xxxxx/xxxx/peticao.pdf')
   },
   anexos: [
     {
-      path: path.resolve('/home/user/Downloads/anexo1.pdf'),
+      path: path.resolve('/xxxxxx/xxxx/xxxx/anexo1.pdf'),
       tipo: 'Outro documento',
       sigiloso: 1
     }
